ba3bfd9d8036428a7737856806b8d8c2468ee5b6
fredikaisbenz <fredikaisbenz@gmail.com>
2025-11-17 15:41:50 +0700

    fix: add type declaration for react-responsive-masonry module

---
+++ types/react-responsive-masonry.d.ts
@@ -0,0 +1,17 @@
+declare module 'react-responsive-masonry' {
+  import { ReactNode } from 'react';
+
+  export interface MasonryProps {
+    columnsCount?: number;
+    gutter?: string;
+    children?: ReactNode;
+  }
+
+  export interface ResponsiveMasonryProps {
+    columnsCountBreakPoints?: { [key: number]: number };
+    children?: ReactNode;
+  }
+
+  export default function Masonry(props: MasonryProps): JSX.Element;
+  export function ResponsiveMasonry(props: ResponsiveMasonryProps): JSX.Element;
+}
